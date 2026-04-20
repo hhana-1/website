d23f245abd81c61c5592f64d52229069e1831756
Hana Hulic <hhulic@f0r4s6.codam.nl>
2026-04-20 02:21:20 +0200

    edit

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ export const projects = [
       {
         id: 1,
           title: "Read Out Loud",
-          description: "Live meetings where people sit in a group setting around tables and read out loud, one person - one paragraph. Everyone has the same reading material. This type of event is supposed to enhance the muscle memory of face and allow easier pronunciation of words. It can be done in any language. It owes its existance to collaboration with student coordinator David Swope from COD who wanted to encourage youth to read more. ADHD friendly.",
+          description: "Live meetings where people sit in a group setting around tables and read out loud, one person - one paragraph. Everyone has the same reading material. This type of event is supposed to enhance the muscle memory of face and allow easier pronunciation of words. It can be done in any language. It owes its existance to collaboration with student coordinator David Swope from COD who wanted to encourage youth to read more. ADHD friendly. Now the legacy of this project is being continued by the students at CODAM and it is being held in the Purple room at 4pm every week.",
           imageGallery: {
             image1: picture,
           },
